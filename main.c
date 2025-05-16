ba77c98f2efd8e1490fb0473f4f2147c16fabce2
David <ddavidmmomanyi8818@gmail.com>
2025-05-16 10:40:06 +0300

    reorganise file structure

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+struct Node{
+    int data;
+    int *next;
+    int *prev;
+};
+
+void printList() {}
+void createNode() {}
+void insertEnd() {}
+void insertBeginning() {}
+void deleteEnd() {}
+void deleteBeginning() {}
+
+
+int main() {
+    printf("HEllo makefile");
+    return 0;
+}
